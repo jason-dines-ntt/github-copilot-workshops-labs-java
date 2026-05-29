94d81775e88987ebae3bdc75c588e6d3586fb88a
jason-dines-ntt <jason.dines@nttdata.com>
2026-05-29 18:57:33 +0100

    Update employee data in SQL and add application properties for configuration

--- lab-02-debugging/java/src/main/resources/data.sql
+++ lab-02-debugging/java/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO employee (name, surname, email) VALUES ('John', 'Doe', 'john.doe@example.com');
-INSERT INTO employee (name, surname, email) VALUES ('Jane', 'Smith', 'jane.smith@example.com');
+INSERT INTO employee (name, surname, email) VALUES ('Ted', 'Crap', 'ted.crap@example.com');
+INSERT INTO employee (name, surname, email) VALUES ('Jane', 'Boobs', 'jane.boobs@example.com');
 INSERT INTO employee (name, surname, email) VALUES ('Bob', 'Jones', 'bob.jones@example.com');
 INSERT INTO employee (name, surname, email) VALUES ('Alice', 'Johnson', 'alice.johnson@example.com');
 INSERT INTO employee (name, surname, email) VALUES ('Charlie', 'Brown', 'charlie.brown@example.com');
